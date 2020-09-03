e00f8a458256816b0f6e81aba2dcbb38d9a3f489
yogeshgadhekar <yogesh_gadhekar@persistent.com>
2020-09-04 00:13:35 +0530

    initial

---
+++ StringOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLibarary
+{
+    public class StringOperation
+    {
+        public static string ToLower(string str)
+        {
+            return str.ToLower();
+        }
+        public static string ToUpper(string str)
+        {
+            return str.ToUpper();
+        }
+    }
+}
